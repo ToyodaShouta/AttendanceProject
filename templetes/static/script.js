23abf104de856ede489bee1b88f9d19cbe533de5
singles_h09 <shiqixingna@gmail.com>
2023-08-04 03:24:08 +0900

    little edited

--- templetes/static/script.js
+++ templetes/static/script.js
@@ -1,26 +1,39 @@
 <script>
+ 
+ function registerAttendance(command) {
+    switch (previousCommand === command) {
+        case 値1:///
+         // 値1と一致する場合の処理
+        break;alert("既に" + command + "登録されています。");
+        case 値2:///
+        // 値2と一致する場合の処理
+        break; alert("既に" + command + "登録されています。");
+    
+     // どの値にも一致しない場合の処理
+     //(default)で打刻完了処理
+        default: var currentTime = new Date().toLocaleString();
+    var message = command + "時間: " + currentTime;
+    ｝}
+    alert(message);
 
+    var previousCommand = null;
 
+    function registerAttendance(command) {
+      if (previousCommand === command) {
+        alert("既に登録されています。");
+        return;
+      }
 
-function registerAttendance(command) {
-switch (previousCommand === command) {
-    case 値1:///
-     // 値1と一致する場合の処理
-    break;alert("既に" + command + "登録されています。");
-    case 値2:///
-    // 値2と一致する場合の処理
-    break; alert("既に" + command + "登録されています。");
+      var currentTime = new Date().toLocaleString();
+      var message = command + "時間: " + currentTime;
 
- // どの値にも一致しない場合の処理
- //(default)で打刻完了処理
-    default: var currentTime = new Date().toLocaleString();
-var message = command + "時間: " + currentTime;
-｝
-alert(message);
-previousCommand = command;
-}
+      alert(message);
 
-const custom-button1 = document.getElementById('custom-button1');
+      previousCommand = command;
+    }
 
-
+    function toggleMenu(menuId) {
+        const menuContent = document.getElementById(menuId);
+        menuContent.classList.toggle('active');
+      }
 </script>
