229c313733f6c98f8dc4dda306fc9402eef90538
singles_h09 <shiqixingna@gmail.com>
2023-08-20 23:13:29 +0900

    0820_last

--- templetes/static/script.js
+++ templetes/static/script.js
@@ -4,8 +4,6 @@
 
     function registerAttendance(command) {
 
-    previousCommand = command;
-
     const button1 = document.getElementById("custom-button1");
     const button2 = document.getElementById("custom-button2");
     const button3 = document.getElementById("custom-button3");
@@ -18,7 +16,7 @@ function checkButtonOrder(clickedButton) {
     case 1:
       if (clickedButton === button1) {
         var message ="時間: " + currentTime + command + "しました。";
-        currentButton = 2;
+        currentButton = 1;
       } else {
         console.log("ボタン1以外のボタンがクリックされました");
       }
@@ -26,7 +24,7 @@ function checkButtonOrder(clickedButton) {
     case 2:
       if (clickedButton === button2) {
         var message ="時間: " + currentTime + command + "しました。";
-        currentButton = 3;
+        currentButton = 2;
       } else {
         console.log("ボタン2以外のボタンがクリックされました");
       }
@@ -34,7 +32,7 @@ function checkButtonOrder(clickedButton) {
     case 3:
       if (clickedButton === button3) {
         var message ="時間: " + currentTime + command + "しました。";
-        currentButton = 1;
+        currentButton = 3;
       } else {
         console.log("ボタン3以外のボタンがクリックされました");
       }
@@ -42,7 +40,7 @@ function checkButtonOrder(clickedButton) {
       case 4:
         if (clickedButton === button4) {
           var message ="時間: " + currentTime + command + "しました。";
-          currentButton = 1;
+          currentButton = 4;
         } else {
           console.log("ボタン4以外のボタンがクリックされました");
         }
@@ -66,25 +64,4 @@ button3.addEventListener("click", () => {
 button4.addEventListener("click", () => {
   checkButtonOrder(button4);
 });
-
-///以下テスト文
-    switch (currentButton) {
-        case 値1:///
-         // 値1と一致する場合の処理
-        break;alert("既に" + command + "しています。");
-        case 値2:///
-        // 値2と一致する場合の処理
-        break; alert("既に" + command + "しています。");
-     // どの値にも一致しない場合の処理
-     //(default)で打刻完了処理
-        default: var currentTime = new Date().toLocaleString();
-    var message ="時間: " + currentTime + command + "しました。";
-    ｝}
-    alert(message);
-    }
-
-    function toggleMenu(menuId) {
-        const menuContent = document.getElementById(menuId);
-        menuContent.classList.toggle('active');
-      }
 </script>
